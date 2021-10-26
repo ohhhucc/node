a32f6604ceea7a6186f6f028248a652e34d16b50
ohhhucc <76487504+ohhhucc@users.noreply.github.com>
2021-10-26 12:16:12 +0300

    add app.post method

--- src/index.js
+++ src/index.js
@@ -10,6 +10,11 @@ app.get('/', (req, res) => {
     res.sendFile('views/index.html', {root: './src'})
 })
 
+app.post('/search', (req, res) => {
+    res.send(req.body);
+    console.log(req.body);
+})
+
 app.listen(3000, () => {
     console.log('Express app on localhost:3000');
 }) ;
